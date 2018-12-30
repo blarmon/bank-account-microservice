488eb70726850c5f4b6912b10459fe433c6c61ac
Siegel <csiegel@dmu.edu>
2018-12-29 19:28:09 -0600

    rolling back last change...

--- account/views.py
+++ account/views.py
@@ -8,11 +8,11 @@ from .serializers import BankAccountSerializer, UserSerializer
 # Create your views here.
 class BankAccountView(viewsets.ModelViewSet):
     queryset = BankAccount.objects.all()
-    serializer_class = BankAccountSerializer(partial=True)
+    serializer_class = BankAccountSerializer
 
 class UserViewSet(viewsets.ModelViewSet):
     """
     API endpoint that allows users to be viewed or edited.
     """
     queryset = User.objects.all().order_by('-date_joined')
-    serializer_class = UserSerializer(partial=True)
+    serializer_class = UserSerializer
